098d8338e5bb59858d9c5f327a5921cf08ecd3c4
Fis <Fis_@mail.ru>
2020-03-04 16:33:21 +0300

    add slider on native js

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,92 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-              }
-        ]
-    });
-  });
+//native js
+let slideIndex = 1, //переменная отвечает за тот слайд который показывается в данный момент
+    slides = document.querySelectorAll('.slider__item'),
+    prev = document.querySelector('.slider__prev'),
+    next = document.querySelector('.slider__next'),
+    dotsWrap = document.querySelector('.slider-dots'),
+    dots = document.querySelectorAll('.dot');
+
+showSlides(slideIndex);
+
+function showSlides(n) { //сюда вместо n попадает 2 из showSlides(slideIndex += n);
+
+    if (n > slides.length) { //если слайды закончились, то возвращаемся к 1 слайду
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length; //возвращаемся к самому последнему слайду
+    }
+
+    slides.forEach((item) => item.style.display = 'none'); //скрываем все слайды 1 способ более современный
+    // for (let i = 0; i < slider.length; i++) { //через цикл
+    //     slider[i].style.display = 'none';
+    dots.forEach((item) => item.classList.remove('dot-active')); //способ более современный
+
+    slides[slideIndex - 1].style.display = 'block'; //конвертация нормальной нумерации в js
+    dots[slideIndex - 1].classList.add('dot-active');
+}
+
+function plusSlides(n) { //будет изменять слайд индекс в зависимости от того в какцю сторону идём
+    showSlides(slideIndex += n); //увеличиваем слайд на 1 и сразу вызываем функцию showSlides уже с 2 как аргумент
+}
+
+//функция которая будет вычисляь текущий слайд и устанавливать его
+function currentSlide(n) {
+    showSlides(slideIndex = n); //когда кликаем например на 4 точку сюда будет передаваться цифра 4 и ф-я showSlides запускается с 4-ой и мы видим 4 слайд
+}
+
+prev.addEventListener('click', function () { //уменьшаем на 1 и идём на слайд назад
+    plusSlides(-1);
+});
+
+next.addEventListener('click', function () {
+    plusSlides(1);
+});
+
+//делегирование событий, если слайды будут автоматически добавляться на страницу, то мы будем генерить новые точки и на них будут висеть уже обработчики событий
+dotsWrap.addEventListener('click', function (event) {
+    for (let i = 0; i < dots.length + 1; i++) {
+        if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+            currentSlide(i);
+        }
+    }
+
+});
+
+
+// slick slider
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         // adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false
+//                 }
+//               }
+//         ]
+//     });
+//   });
+
+
+// const slider = tns({
+//     container: '.carousel__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     controls: false,
+//     nav: false
+// });
+
+// document.querySelector('.prev').addEventListener('click', function () {
+//     slider.goTo('prev');
+//   });
+//   document.querySelector('.next').addEventListener('click', function () {
+//     slider.goTo('next');
+//   });

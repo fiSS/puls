85d72beaf741c8b616f84a7f0238c3f9be3be2fe
Fis <Fis_@mail.ru>
2020-03-21 16:13:07 +0300

    add modal window on jquery

--- src/js/script.js
+++ src/js/script.js
@@ -76,7 +76,28 @@ $('.catalog-item__back').each(function(i) {
         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
     });
+
+    //modal
+    //открывам модал
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn("slow");
+    });
+    //закрываем модал
+    $('.modal__close').on('click', function() {
+        $('.overlay, #conultation, #thanks, #order').fadeOut('slow');
+    });
+    
+    $('.button_mini').each(function(i){
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
 });
+
+
+
 
 
 // slick slider

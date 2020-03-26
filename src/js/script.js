f0c796b9e260c3296f0187270ae07d1ebae5ca4e
Fis <Fis_@mail.ru>
2020-03-26 14:26:34 +0300

    add form sending date from inputs, and smoth scroll

--- src/js/script.js
+++ src/js/script.js
@@ -97,6 +97,36 @@ $('.catalog-item__back').each(function(i) {
     //mask input number
     $("input[name=phone]").mask("+7(999) 999-99-99");
 
+    //send mail ajax
+    $('form').submit(function(event){
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").value("");
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //smooth scroll and pageUp
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
 });
 
 
